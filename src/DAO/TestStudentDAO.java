601a6f4c8c2060471628ee780f1068896cc24dc5
pdaloia <48640498+pdaloia@users.noreply.github.com>
2019-03-20 13:09:26 -0400

    Add files via upload
    added test servlet and dao

---
+++ src/DAO/TestStudentDAO.java
@@ -0,0 +1,29 @@
+package DAO;
+
+import java.sql.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class TestStudentDAO {
+	
+	public static void main(String[] args) throws SQLException {
+		try {
+			DataSource ds = (DataSource) (new InitialContext()).lookup("java:/comp/env/jdbc/EECS");
+			Connection con = ds.getConnection();
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM PO");
+			while(rs.next()) {
+				String phone= rs.getString("FNAME");
+				System.out.println("\t" + phone + "\t ");
+			}//end while loop
+			con.close();
+			rs.close();
+			stmt.close();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+}
