fd8e54c86e9f8c1501598782488c4696d54807b2
pdaloia <48640498+pdaloia@users.noreply.github.com>
2019-03-28 13:42:48 -0400

    Add files via upload
    new model with new functions

--- src/model/model.java
+++ src/model/model.java
@@ -13,12 +13,13 @@ import bean.POBean;
 import bean.AddressBean;
 import bean.BookBean;
 import bean.POItemBean;
-
+import bean.ReviewBean;
 import DAO.AddressDAO;
 import DAO.BookDAO;
 import DAO.PODAO;
 import DAO.POItemDAO;
 import DAO.VisitEventDAO;
+import DAO.ReviewDAO;
 
 import java.util.Map;
 
@@ -26,11 +27,18 @@ import java.util.Map;
 public class model {
 
 private BookDAO bookDAO;
+private ReviewDAO reviewDAO;
 
 	public model(){
 		
 		try {
 			bookDAO = new BookDAO();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			reviewDAO = new ReviewDAO();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -51,6 +59,28 @@ private BookDAO bookDAO;
 		Map<String, BookBean> rv = new HashMap<String, BookBean>();
 		rv = bookDAO.retrieveBooksBySearch(searchString);
 		return rv;
+		
+	}
+	
+	public Map<String, BookBean> retrieveBooksByBID(String searchBID) throws Exception{
+		
+		Map<String, BookBean> rv = new HashMap<String, BookBean>();
+		rv = bookDAO.retrieveBooksByBID(searchBID);
+		return rv;
+		
+	}
+	
+	public Map<String, ReviewBean> retrieveReviewsByBID(String searchBID) throws Exception{
+		
+		Map<String, ReviewBean> rv = new HashMap<String, ReviewBean>();
+		rv = reviewDAO.retrieveReviewsByBid(searchBID);
+		return rv;
+		
+	}
+	
+	public void addReview(String bid, String review, String email, int rating) throws Exception{
+		
+		reviewDAO.addReview(bid, review, email, rating);
 		
 	}
 
