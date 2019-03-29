20eeac13d7f46a29175aea2c980e060c3d06af4e
pdaloia <48640498+pdaloia@users.noreply.github.com>
2019-03-29 14:07:04 -0400

    Updated model to include features for AccountDAO (login and logout)

--- src/model/model.java
+++ src/model/model.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.StringWriter;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import bean.VisitEventBean;
 import bean.POBean;
+import bean.AccountBean;
 import bean.AddressBean;
 import bean.BookBean;
 import bean.POItemBean;
@@ -20,6 +22,7 @@ import DAO.PODAO;
 import DAO.POItemDAO;
 import DAO.VisitEventDAO;
 import DAO.ReviewDAO;
+import DAO.AccountDAO;
 
 import java.util.Map;
 
@@ -28,6 +31,7 @@ public class model {
 
 private BookDAO bookDAO;
 private ReviewDAO reviewDAO;
+private AccountDAO accountDAO;
 
 	public model(){
 		
@@ -39,6 +43,12 @@ private ReviewDAO reviewDAO;
 		}
 		try {
 			reviewDAO = new ReviewDAO();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			accountDAO = new AccountDAO();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -81,6 +91,18 @@ private ReviewDAO reviewDAO;
 	public void addReview(String bid, String review, String email, int rating) throws Exception{
 		
 		reviewDAO.addReview(bid, review, email, rating);
+		
+	}
+	
+	public boolean checkForUser(String givenUsername) throws Exception{
+		
+		return accountDAO.checkForUser(givenUsername);
+		
+	}
+	
+	public AccountBean retrieveAccountByUsername(String givenUsername, String givenPassword) throws Exception{
+		
+		return accountDAO.retrieveAccountByUsername(givenUsername, givenPassword);
 		
 	}
 
