7c40315f4e659e50258dc08c64f85d705617933e
pdaloia <48640498+pdaloia@users.noreply.github.com>
2019-03-29 14:04:57 -0400

    Add files via upload
    Updated DAOs, now has accountDAO for login and logout feature

--- src/DAO/AccountDAO.java
+++ src/DAO/AccountDAO.java
@@ -48,5 +48,63 @@ public class AccountDAO {
 		
 		return rv;
 	}
+	
+	public boolean checkForUser(String givenUsername) throws SQLException{
+		
+		boolean exists;
+		
+		String query = "select * from accounts where username='" + givenUsername + "'";
+
+		Connection con = this.ds.getConnection();
+		PreparedStatement p = con.prepareStatement(query);
+		ResultSet r = p.executeQuery();
+		
+		int size = 0;
+		
+		while(r.next()) {
+			size++;
+		}
+		
+		if(size > 0) {
+			exists = true;
+		}
+		else {
+			exists = false;
+		}
+		
+		r.close();
+		p.close();
+		con.close();
+		
+		return exists;
+		
+	}
+	
+	public AccountBean retrieveAccountByUsername(String givenUsername, String givenPassword) throws SQLException{
+		String query = "select * from accounts where username='" + givenUsername + "' and password='" + givenPassword + "'";
+		Map<String, AccountBean> rv = new HashMap<String, AccountBean>();
+		Connection con = this.ds.getConnection();
+		PreparedStatement p = con.prepareStatement(query);
+		ResultSet r = p.executeQuery();
+		
+		while (r.next()){
+			
+			String username = r.getString("USERNAME");
+			String fname = r.getString("FNAME");
+			String lname = r.getString("LNAME");
+			String email = r.getString("EMAIL");
+			String password = r.getString("PASSWORD");
+			AccountBean current;
+			current = new AccountBean(username, fname, lname, email, password);
+			rv.put(username, current);
+			
+		}
+		
+		r.close();
+		p.close();
+		con.close();
+		
+		return rv.get(givenUsername);
+	}
 
 }
