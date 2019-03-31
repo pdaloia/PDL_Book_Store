cdf95c8852a7a029397672e2b8c157937e142bb5
ammar1414 <33293211+ammar1414@users.noreply.github.com>
2019-03-31 10:18:17 -0400

    Updated model

--- src/model/model.java
+++ src/model/model.java
@@ -1,132 +1,146 @@
 package model;
 
 import java.util.HashMap;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.StringWriter;
+import bean.AccountBean;
+import bean.BookBean;
+import bean.CartBean;
+import bean.ReviewBean;
+import DAO.BookDAO;
+import DAO.CartDAO;
+import DAO.ReviewDAO;
+import DAO.AccountDAO;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
-import bean.VisitEventBean;
-import bean.POBean;
-import bean.AccountBean;
-import bean.AddressBean;
-import bean.BookBean;
-import bean.POItemBean;
-import bean.ReviewBean;
-import DAO.AddressDAO;
-import DAO.BookDAO;
-import DAO.PODAO;
-import DAO.POItemDAO;
-import DAO.VisitEventDAO;
-import DAO.ReviewDAO;
-import DAO.AccountDAO;
 
 import java.util.Map;
-
 
 public class model {
 
-private BookDAO bookDAO;
-private ReviewDAO reviewDAO;
-private AccountDAO accountDAO;
-private CartDAO cartDAO;
+    private BookDAO bookDAO;
+    private ReviewDAO reviewDAO;
+    private AccountDAO accountDAO;
+    private CartDAO cartDAO;
 
-	public model(){
-		
-		try {
-			bookDAO = new BookDAO();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			reviewDAO = new ReviewDAO();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			accountDAO = new AccountDAO();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			cartDAO = new CartDAO();
-			
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public Map<String, BookBean> retrieveBooksByCategory(String categoryToSearch) throws Exception{
-		
-		Map<String, BookBean> rv = new HashMap<String, BookBean>();
-		rv = bookDAO.retrieveBooksByCategory(categoryToSearch);
-		return rv;
-		
-	}
-	
-	public Map<String, BookBean> retrieveBooksBySearch(String searchString) throws Exception{
-		
-		Map<String, BookBean> rv = new HashMap<String, BookBean>();
-		rv = bookDAO.retrieveBooksBySearch(searchString);
-		return rv;
-		
-	}
-	
-	public Map<String, BookBean> retrieveBooksByBID(String searchBID) throws Exception{
-		
-		Map<String, BookBean> rv = new HashMap<String, BookBean>();
-		rv = bookDAO.retrieveBooksByBID(searchBID);
-		return rv;
-		
-	}
-	
-	public Map<String, ReviewBean> retrieveReviewsByBID(String searchBID) throws Exception{
-		
-		Map<String, ReviewBean> rv = new HashMap<String, ReviewBean>();
-		rv = reviewDAO.retrieveReviewsByBid(searchBID);
-		return rv;
-		
-	}
-	
-	public void addReview(String bid, String review, String email, int rating) throws Exception{
-		
-		reviewDAO.addReview(bid, review, email, rating);
-		
-	}
-	
-	public boolean checkForUser(String givenUsername) throws Exception{
-		
-		return accountDAO.checkForUser(givenUsername);
-		
-	}
-	
-	public AccountBean retrieveAccountByUsername(String givenUsername, String givenPassword) throws Exception{
-		
-		return accountDAO.retrieveAccountByUsername(givenUsername, givenPassword);
-		
-	}
-	
-	public void addNewAccount(String username, String fname, String lname, String email, String password) throws Exception{
-		
-		accountDAO.addNewAccount(username, fname, lname, email, password);
-		
-	}
-	public Map<String, CartBean> retrieveCart() throws Exception{
-		
-		Map<String, CartBean> rc = new HashMap<String, CartBean>();
-		
-		rc = cartDAO.retrieveAllCarts();
-		return rc;
-		
-	}
-	
+    public model() {
 
+        try {
+            bookDAO = new BookDAO();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            reviewDAO = new ReviewDAO();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            accountDAO = new AccountDAO();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            cartDAO = new CartDAO();
+
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    public Map<String, BookBean> retrieveBooksByCategory(String categoryToSearch) throws Exception {
+
+        Map<String, BookBean> rv = new HashMap<String, BookBean>();
+        rv = bookDAO.retrieveBooksByCategory(categoryToSearch);
+        return rv;
+
+    }
+
+    public Map<String, BookBean> retrieveBooksBySearch(String searchString) throws Exception {
+
+        Map<String, BookBean> rv = new HashMap<String, BookBean>();
+        rv = bookDAO.retrieveBooksBySearch(searchString);
+        return rv;
+
+    }
+
+    public Map<String, BookBean> retrieveBooksByBID(String searchBID) throws Exception {
+
+        Map<String, BookBean> rv = new HashMap<String, BookBean>();
+        rv = bookDAO.retrieveBooksByBID(searchBID);
+        return rv;
+
+    }
+
+    public Map<String, ReviewBean> retrieveReviewsByBID(String searchBID) throws Exception {
+
+        Map<String, ReviewBean> rv = new HashMap<String, ReviewBean>();
+        rv = reviewDAO.retrieveReviewsByBid(searchBID);
+        return rv;
+
+    }
+
+    public void addReview(String bid, String review, String email, int rating) throws Exception {
+
+        reviewDAO.addReview(bid, review, email, rating);
+
+    }
+
+    public boolean checkForUser(String givenUsername) throws Exception {
+
+        return accountDAO.checkForUser(givenUsername);
+
+    }
+
+    public AccountBean retrieveAccountByUsername(String givenUsername, String givenPassword) throws Exception {
+
+        return accountDAO.retrieveAccountByUsername(givenUsername, givenPassword);
+
+    }
+
+    
+    
+    
+    /********** Cart Module from here *****************/
+    
+    public List<CartBean> retrieveUserCart(String username) {
+            try{
+              return  cartDAO.getCartByUser(username);
+            }catch(SQLException e){
+               e.printStackTrace();
+            }
+            return new ArrayList<>();
+    }
+    
+    public void addProductToCart(String bid,String title, String username,float price, int quantity ){
+        CartBean b = this.getCartProductByProductId(bid, username);
+        if(null==b){
+            cartDAO.addProductToCart(bid, title, username, price, quantity);
+        }else{
+            cartDAO.updateCartProduct(bid, username, price, quantity,true);
+        }
+        
+        
+    }
+    
+    public CartBean getCartProductByProductId(String bid,String username){
+        return cartDAO.getCartProductByProductId(bid,username);
+    }
+    
+    public void removeItemFromCart(String bid,String username){
+         cartDAO.removeItem(bid,username);
+    }
+    
+    public void updateCartProduct(String bid, String username,float price,int quantity){
+       cartDAO.updateCartProduct(bid, username, price, quantity,false);
+    }
+    
+    public int countCartItem(String username){
+        
+       return cartDAO.countCartItem(username);
+    }
 }
