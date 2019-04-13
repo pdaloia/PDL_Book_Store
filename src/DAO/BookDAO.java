ec62874b2e2415b74b20202695334112643d006d
pdaloia <48640498+pdaloia@users.noreply.github.com>
2019-04-13 18:40:41 -0400

    Fixed BookDAO to work with ammars commit

--- src/DAO/BookDAO.java
+++ src/DAO/BookDAO.java
@@ -50,6 +50,33 @@ public class BookDAO {
 		return rv;
 	}
 	
+	public Map<String, BookBean> retrieveAllBooksMap() throws SQLException{
+		String query = "select * from book";
+		Map<String, BookBean> rv = new HashMap<String, BookBean>();
+		Connection con = this.ds.getConnection();
+		PreparedStatement p = con.prepareStatement(query);
+		ResultSet r = p.executeQuery();
+		
+		while (r.next()){
+			
+			String bid = r.getString("BID");
+			String title = r.getString("TITLE");
+			String price = r.getString("PRICE");
+			String author = "";
+			String category = r.getString("CATEGORY");
+			BookBean current;
+			current = new BookBean(bid, title, price, author, category);
+			rv.put(bid, current);
+			
+		}
+		
+		r.close();
+		p.close();
+		con.close();
+		
+		return rv;
+	}
+	
 	public Map<String, BookBean> retrieveBooksByCategory(String categoryToSearch) throws SQLException{
 		
 		String query = "select * from book where category like '" + categoryToSearch + "'";
